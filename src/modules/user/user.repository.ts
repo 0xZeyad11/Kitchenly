2009ebb3ccfa608f6ea89d8d7ed1857d3fed92da
Zeyad <zorro.zizo3@gmail.com>
2025-10-30 22:51:07 +0300

    Add Geospatial functions for getting nearby chefs relative to the current user

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,15 +1,14 @@
 import prisma from "../../../prisma/db";
 import { User, Prisma } from "@prisma/client";
 import { sendPrismaError } from "../../common/middelware/errorhandler.middleware";
-import bcrypt from 'bcrypt';
 
-export async function getUserByEmailAuth(data: string): Promise<any>{
-  try{
+export async function getUserByEmailAuth(data: string): Promise<any> {
+  try {
     return await prisma.user.findUniqueOrThrow({
       where: { email: data },
-      select: { id: true,  name: true, email: true  , password: true},
-    });  
-  }catch(error){
+      select: { id: true, name: true, email: true, password: true },
+    });
+  } catch (error) {
     sendPrismaError(error);
   }
 }
@@ -29,7 +28,7 @@ export async function getAllUsers(
     return await prisma.user.findMany({
       ...options,
     });
-  } catch (error) {throw sendPrismaError(error)}
+  } catch (error) { throw sendPrismaError(error) }
 }
 
 export async function getAllChiefs(
@@ -89,3 +88,64 @@ export async function getAllCustomers(
     throw sendPrismaError(error);
   }
 }
+
+export async function getNearbyChefs(
+  userid: string,
+  distanceMeters: number
+): Promise<(User & { distance_meters: number })[]> {
+  try {
+    const chefs = await prisma.$queryRaw<
+      (User & { distance_meters: number })[]
+    >
+      `
+SELECT 
+u.*,
+ST_DistanceSphere(u.location, ref.location) AS distance_meters
+FROM "User" u
+JOIN "User" ref ON ref.id = ${userid}
+WHERE u.id <> ref.id
+AND u.role = 'CHEF'
+AND ST_DistanceSphere(u.location, ref.location) <= ${distanceMeters}
+ORDER BY distance_meters ASC;
+`;
+
+    return chefs;
+  } catch (error) {
+    throw sendPrismaError(error);
+  }
+}
+
+export async function setUserLocation(userid: string, lat: number, lng: number): Promise<void> {
+  try {
+    await prisma.$executeRaw
+      `
+UPDATE  "User" 
+SET "location" = ST_SetSRID(ST_MakePoint(${lng},${lat}) , 4326)
+WHERE id = ${userid} ;
+` ;
+  } catch (error) {
+    throw sendPrismaError(error);
+  }
+}
+
+export async function getNearstChef(userid: string, distance: number, NumberOfChefs?: number): Promise<User> {
+  try {
+    const no_of_chefs = NumberOfChefs === 0 ? 0 : NumberOfChefs;
+    return await prisma.$queryRaw<User>`
+SELECT 
+u.*,
+ST_DistanceSphere(u.location, ref.location) AS distance_meters
+FROM "User" u
+JOIN "User" ref ON ref.id = ${userid}
+WHERE u.id <> ref.id
+AND u.role = 'CHEF'
+AND ST_DistanceSphere(u.location, ref.location) <= ${distance}
+ORDER BY distance_meters ASC
+LIMIT ${no_of_chefs};
+`
+
+  } catch (error) {
+    throw sendPrismaError(error);
+  }
+}
+
